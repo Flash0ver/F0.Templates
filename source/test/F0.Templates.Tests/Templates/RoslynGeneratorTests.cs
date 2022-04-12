9ca2482aba635df41ce39ccc0df1f2efb82e36db
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-04-12 04:37:30 +0200

    test: add tests

---
+++ source/test/F0.Templates.Tests/Templates/RoslynGeneratorTests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Boxed.DotnetNewTest;
+using Xunit;
+
+namespace F0.Tests.Templates
+{
+	public class RoslynGeneratorTests : IAsyncLifetime
+	{
+		private const string templateName = "roslyngenerator";
+
+		public Task InitializeAsync()
+		{
+			return DotnetNew.InstallAsync<RoslynGeneratorTests>("F0.Templates.sln");
+		}
+
+		public Task DisposeAsync()
+		{
+			return DotnetNew.UninstallAsync<RoslynGeneratorTests>("F0.Templates.sln");
+		}
+
+		[Theory]
+		[InlineData("Roslyn3_8_MSTest", "roslyn=3.8", "testing-framework=mstest", "no-restore=false")]
+		[InlineData("Roslyn3_9_NUnit", "roslyn=3.9", "testing-framework=nunit", "no-restore=false")]
+		[InlineData("Roslyn4_0_xUnit", "roslyn=4.0", "testing-framework=xunit", "no-restore=false")]
+		public async Task DotnetNew_Async(string projectName, params string[] arguments)
+		{
+			await using (var tempDirectory = TempDirectory.NewTempDirectory())
+			{
+				IDictionary<string, string> dictionary = ToArguments(arguments);
+
+				Project project = await tempDirectory.DotnetNewAsync(templateName, projectName, dictionary);
+
+				await project.DotnetRestoreAsync();
+				await project.DotnetBuildAsync();
+				await project.DotnetTestAsync();
+			}
+		}
+
+		private static IDictionary<string, string> ToArguments(string[] arguments)
+		{
+			return arguments
+				.Select(x => x.Split('=', StringSplitOptions.RemoveEmptyEntries))
+				.ToDictionary(x => x.First(), x => x.Last());
+		}
+	}
+}
